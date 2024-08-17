4ef5dab1609b1bd6b6d1a6c129280c1288c8b434
clay <claytonharting+github@gmail.com>
2024-08-17 14:22:10 -0400

    added camera infrastructure

---
+++ camera_controller.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# needs two modes: tracking mech and tracking fairy
+# needs to be able to transition between those modes
+# tracking code should be essentially the same, the only difference being the offset from the target object
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
