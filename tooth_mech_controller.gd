a7ea4a7b640d97bb3292262353330d5dd5aec2e5
clay <claytonharting+github@gmail.com>
2024-08-17 14:32:08 -0400

    added tooth mech object

---
+++ tooth_mech_controller.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_down", "ui_up"))
+	
