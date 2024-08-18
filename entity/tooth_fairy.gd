872dc0c6449bd952c3075247d835b55d0c952558
clay <claytonharting+github@gmail.com>
2024-08-17 20:23:36 -0400

    added placeholder art, moved camera to first person perspective of fairy, implemented new movement scheme

--- entity/tooth_fairy.gd
+++ entity/tooth_fairy.gd
@@ -1,22 +1,50 @@
 class_name ToothFairy extends Entity
 
 @export var starting_distance = 70
-@export var speed: float = 200
-@export var boost_speed: float
+@export var speed: float = 20
+@export var boost_speed: float = 40
+@export var strafe_speed: float = 5
 @export var tooth_mech: Node3D
 
-@export var velocity = Vector3()
+#@export var velocity = Vector3()
+@export var max_turn_speed: float = 0.001
+var facing: float = 0
+var target_facing: float = 0
+@export var turning_sensitivity: float = 0.0001
+
+var boosting = false
 
 func _ready() -> void:
-	self.position = tooth_mech.position + Vector3(0, -self.starting_distance, 0)
-	self.velocity = Vector2(speed, 0)
+	pass
+	#self.position = tooth_mech.position + Vector3(0, 0, -self.starting_distance)
+	#self.velocity = Vector3(speed, 0, 0)
 
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		#print("Mouse Click/Unclick at: ", event.position)
+		if event.button_index == 0:
+			boosting = event.pressed
+		print("EVENT DETAILS %s %s " % [event.button_index, event.pressed])
+	elif event is InputEventMouseMotion:
+		#print("Mouse Motion at: ", event.position)
+		print("EVENT DETAILS %s %s %s" % [event.screen_velocity * turning_sensitivity, facing, target_facing])
+		target_facing -= event.screen_velocity.x * turning_sensitivity
+		#target_facing %= TAU
+	
 
+# redo as simple "always move forward, mouse left/right change facing dir, A/D strafe"
 func _process(delta: float) -> void:
-	var input_direction = Vector2(Input.get_axis("ui_left", "ui_right"), -Input.get_axis("ui_down", "ui_up"))
-	var circling_direction = tooth_mech.position - self.position
+	var forwards = -Vector3(sin(facing), 0, sin(facing))
+	var strafe_direction = Input.get_axis("ui_left", "ui_right") * forwards
+	self.position += strafe_direction * strafe_speed * delta
 	
-	velocity += circling_direction
+	self.position += (boost_speed if boosting else speed) * forwards * delta
 	
-	self.position += self.velocity * delta
-	self.position += input_direction * speed * delta
+	var last_facing = facing
+	if abs(facing-target_facing) <= max_turn_speed:
+		facing = target_facing
+	else:
+		facing = lerp(facing, target_facing, max(1, max_turn_speed*delta))
+	rotate_y(facing - last_facing)
+	
