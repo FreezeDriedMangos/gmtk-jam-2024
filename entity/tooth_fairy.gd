dd78a981a9740e758600f6b6876175c6511106c6
clay <claytonharting+github@gmail.com>
2024-08-18 22:14:44 -0400

    better drifting??? maybe???

--- entity/tooth_fairy.gd
+++ entity/tooth_fairy.gd
@@ -6,12 +6,10 @@ class_name ToothFairy extends Entity
 @export var tooth_mech: Node3D
 
 @export var velocity = Vector3()
-@export var max_friction = 0.99
-@export var min_friction = 0.9
-@export var max_turn_speed: float = 0.05
+@export var max_friction: float = 1.9
+@export var min_friction: float = 0.9
+@export var max_turn_speed: float = 0.09
 var facing: float = 0
-#var target_facing: float = 0
-@export var turning_sensitivity: float = 0.001
 
 @export var camera: Camera3D
 
@@ -56,45 +54,15 @@ func raycast_from_mouse():
 	return result["position"]
 
 
-var updateCount = 0
-
 func _process(delta: float) -> void:
 	#
-	# move
+	# determine desired movement direction
 	#
 	
-	## https://stackoverflow.com/a/74972847
-	#var mouse_coords = camera.get_viewport().get_mouse_position()
-	#
-	#var from = camera.project_ray_origin(mouse_coords)
-	#var to = from + camera.project_ray_normal(mouse_coords) * 1_000
-	##var space = get_world_3d().direct_space_state #get_world().direct_space_state
-	##
-	### ( Vector3 from, Vector3 to, Array exclude=[ ], int collision_mask=2147483647, bool collide_with_bodies=true, bool collide_with_areas=false )
-	### the only thing in this layer should be our world area adjusted at character eye level
-	###var intersection = space.intersect_ray(from, to, [], 32768, false, true)
-	##var query = PhysicsRayQueryParameters3D.create(from, to)
-	##var intersection = get_world_3d().direct_space_state.intersect_ray(query)
-	#
-	#var ray = to - from
-	#var intersection = Vector3( )
-	
-	# NOTE: this isn't an accurate world pos for the mouse
-	# TODO: make it accurate
-	# TODO: translate it by (player.pos - camera.pos)
-	#var mouseWorldPos = Vector3(mouseScreenPos_relativeToCenterOfScreen.x, 0, mouseScreenPos_relativeToCenterOfScreen.y) + camera.position
 	var mouseWorldPos = raycast_from_mouse()
-	
 	var mouseRelPos = mouseWorldPos - self.position
 	
-	updateCount += 1
-	if updateCount % 30 == 0:
-		print("------- rel, world  -------")
-		print(mouseRelPos)
-		print(mouseWorldPos)
-		updateCount = 0
-	
-	var desired_dir = Vector3(mouseRelPos.x, 0, mouseRelPos.z) #- self.position
+	var desired_dir = Vector3(mouseRelPos.x, 0, mouseRelPos.z)
 	
 	# 
 	# turn
@@ -110,13 +78,11 @@ func _process(delta: float) -> void:
 	# move
 	#
 	
-	#move forward not towards mouse directly
-	
-	#print("%s %s %s %s" % [desired_facing, facing, facing_delta, max_facing_delta/PI]) # why does this get all the way up to 6
-	var friction_scale = clamp(2* max_facing_delta / PI, 0, 1) ** 2
+	# move in current forwards direction, not directly towards mouse
+	var friction_scale = clamp(2* max_facing_delta / PI, 0, 1)
 	var friction = lerp(min_friction, max_friction, friction_scale)
-	#self.velocity *= friction
-	#self.velocity += (1-friction) * desired_dir.normalized()*speed	
+	friction = clampf(friction, 0, 1)
+	
 	var speed = boost_speed if boosting else base_move_speed
 	self.velocity = self.velocity*friction + (1-friction)*Vector3(sin(facing), 0, cos(facing))*speed
 		
